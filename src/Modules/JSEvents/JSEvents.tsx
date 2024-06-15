10263dcde86a2b7f97f01e02bdb513cb3e06aca6
Vitalii Zakharchuk <vit.zkh@gmail.com>
2024-06-15 13:11:54 +0300

    js events

--- src/Modules/JSEvents/JSEvents.tsx
+++ src/Modules/JSEvents/JSEvents.tsx
@@ -7,7 +7,7 @@ export const JSEvents: React.FC = () => {
 			{jsEvents.map((item) => {
 				return (
 					<div className="flex flex-col md:flex-row md:items-center">
-						<div className="text-2xl font-bold w-[160px]">
+						<div className="text-2xl font-bold w-[180px]">
 							{item.name}
 						</div>
 						<div className="w-4/5">{item.text}</div>
--- src/Modules/JSEvents/constants.ts
+++ src/Modules/JSEvents/constants.ts
@@ -1,5 +1,31 @@
 export const jsEvents = [
 	{
+		name: 'Drag',
+		text:
+			'ondrag, ondragend, ondragenter, ondragleave, ondragover,' +
+			'ondragstart, ondrop',
+	},
+	{
+		name: 'Form',
+		text:
+			'onblur, onchange, onfocus, onfocusin, onfocusout, oninput,' +
+			'oninvalid, onreset, onsearch, onselect, onsubmit',
+	},
+	{
+		name: 'Media',
+		text:
+			'onabort, oncanplay, oncanplaythrough, ondurationchange, onended, onerror, onloadeddata,' +
+			'onloadedmetadata,' +
+			'onloadstart, onpause, onplay, onplaying, onprogress, onratechange, onseeked, onseeking, onstalled, onsuspend, ontimeupdate, onvolumechange, onwaiting',
+	},
+	{
+		name: 'Frame',
+		text:
+			'onabort, onbeforeunload, onerror, onhashchange, ' +
+			'onload, onpageshow, onpagehide, onresize, onscroll, ' +
+			'onunload',
+	},
+	{
 		name: 'Mouse',
 		text:
 			'onclick, oncontextmenu, ondblclick, onmousedown,' +
@@ -11,26 +37,12 @@ export const jsEvents = [
 		text: 'onkeydown, onkeypress, onkeyup',
 	},
 	{
-		name: 'Frame',
-		text:
-			'onabort, onbeforeunload, onerror, onhashchange, ' +
-			'onload, onpageshow, onpagehide, onresize, onscroll, ' +
-			'onunload',
-	},
-	{
-		name: 'Form',
-		text:
-			'onblur, onchange, onfocus, onfocusin, onfocusout, oninput,' +
-			'oninvalid, onreset, onsearch, onselect, onsubmit',
-	},
-	{
-		name: 'Drag',
-		text:
-			'ondrag, ondragend, ondragenter, ondragleave, ondragover,' +
-			'ondragstart, ondrop',
-	},
-	{
 		name: 'Animation',
 		text: 'animationend, animationiteration, animationstar',
+	},
+
+	{
+		name: 'Miscellaneous',
+		text: 'transitionend, onmessage, onmousewheel, ononline, onoffline, onpopstate, onshow, onstorage, ontoggle, onwheel, ontouchcancel, ontouchend, ontouchmove, ontouchstart',
 	},
 ]
